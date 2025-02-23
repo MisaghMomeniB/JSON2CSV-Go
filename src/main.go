b65ab678dac2f1c4bed6aa8a058ea601dea4d0cf
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-23 23:23:17 +0330

    Convert JSON to a slice of maps

--- src/main.go
+++ src/main.go
@@ -15,3 +15,11 @@ func main() {
 		fmt.Println("Error reading JSON file:", err)
 		return
 	}
+
+	// Convert JSON to a slice of maps
+	var records []map[string]interface{}
+	err = json.Unmarshal(jsonFile, &records)
+	if err != nil {
+		fmt.Println("Error processing JSON:", err)
+		return
+	}
